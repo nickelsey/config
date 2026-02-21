2ae835390ef4060e4c1f6589b61b334ef0bc1f40
Nick Elsey <nick@newton.local>
2026-02-20 20:36:29 -0500

    add colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -802,20 +802,93 @@ require('lazy').setup({
         -- change the command in the config to whatever the name of that colorscheme is.
         --
         -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-        'folke/tokyonight.nvim',
-        priority = 1000, -- Make sure to load this before all the other start plugins.
+        'rose-pine/neovim',
+        priority = 1000,         -- Make sure to load this before all the other start plugins.
         config = function()
             ---@diagnostic disable-next-line: missing-fields
-            require('tokyonight').setup {
-                styles = {
-                    comments = { italic = false }, -- Disable italics in comments
+            require('rose-pine').setup {
+                variant = 'auto',              -- auto, main, moon, or dawn
+                dark_variant = 'main',         -- main, moon, or dawn
+                dim_inactive_windows = false,
+                extend_background_behind_borders = true,
+
+                enable = {
+                    terminal = true,
+                    legacy_highlights = true,         -- Improve compatibility for previous versions of Neovim
+                    migrations = true,                -- Handle deprecated options automatically
                 },
+
+                styles = {
+                    bold = true,
+                    italic = true,
+                    transparency = false,
+                },
+
+                groups = {
+                    border = 'muted',
+                    link = 'iris',
+                    panel = 'surface',
+
+                    error = 'love',
+                    hint = 'iris',
+                    info = 'foam',
+                    note = 'pine',
+                    todo = 'rose',
+                    warn = 'gold',
+
+                    git_add = 'foam',
+                    git_change = 'rose',
+                    git_delete = 'love',
+                    git_dirty = 'rose',
+                    git_ignore = 'muted',
+                    git_merge = 'iris',
+                    git_rename = 'pine',
+                    git_stage = 'iris',
+                    git_text = 'rose',
+                    git_untracked = 'subtle',
+
+                    h1 = 'iris',
+                    h2 = 'foam',
+                    h3 = 'rose',
+                    h4 = 'gold',
+                    h5 = 'pine',
+                    h6 = 'foam',
+                },
+
+                palette = {
+                    -- Override the builtin palette per variant
+                    -- moon = {
+                    --     base = '#18191a',
+                    --     overlay = '#363738',
+                    -- },
+                },
+
+                -- NOTE: Highlight groups are extended (merged) by default. Disable this
+                -- per group via `inherit = false`
+                highlight_groups = {
+                    -- Comment = { fg = "foam" },
+                    -- StatusLine = { fg = "love", bg = "love", blend = 15 },
+                    -- VertSplit = { fg = "muted", bg = "muted" },
+                    -- Visual = { fg = "base", bg = "text", inherit = false },
+                },
+
+                before_highlight = function(group, highlight, palette)
+                    -- Disable all undercurls
+                    -- if highlight.undercurl then
+                    --     highlight.undercurl = false
+                    -- end
+                    --
+                    -- Change palette colour
+                    -- if highlight.fg == palette.pine then
+                    --     highlight.fg = palette.foam
+                    -- end
+                end,
             }
 
             -- Load the colorscheme here.
             -- Like many other themes, this one has different styles, and you could load
             -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-            vim.cmd.colorscheme 'tokyonight-night'
+            vim.cmd.colorscheme 'rose-pine-moon'
         end,
     },
 
